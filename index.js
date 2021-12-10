0c679af52ee19fd6988779541402f64b68bdefb8
ludico1959 <ludico1959@gmail.com>
2021-12-10 16:26:14 -0300

    create a module for the replace template function

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
-/////////////////////////////////////////////////////////////////////////////////////////
-// FILES
-
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-
-// const textOut = `This is what we know about avocado: ${textIn}\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File written!');
-
-// Non-blocking, asynchronous way
-// fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
-//     if (error) return console.log('ERROR 🪔')
-
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-//         console.log(data2);
-//         fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
-//             console.log(data3);
-
-//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', error => {
-//                 console.log('Your file has been written 🎃')
-//             });
-//         });
-//     });
-// });
-// console.log('Will read file!');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // SERVER
-const replaceTemplate = (template, product) => {
-    // Use regular expression to replace all placeholder instead of just the first one.
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-    return output;
-}
-
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
